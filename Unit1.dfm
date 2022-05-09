1e1e7a1bb54df4fe76e303b5b78dad0029a1dcc0
Sergey Kharitonov <60604804+DevSergo@users.noreply.github.com>
2022-05-09 17:49:05 +0300

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,49 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = #1055#1086#1083#1080#1084#1086#1088#1092#1080#1079#1084' '#1074' Delphi'
+  ClientHeight = 201
+  ClientWidth = 447
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poDesktopCenter
+  PixelsPerInch = 96
+  TextHeight = 13
+  object btAddIntegerValues: TButton
+    Left = 16
+    Top = 32
+    Width = 137
+    Height = 41
+    Caption = #1057#1083#1086#1078#1080#1090#1100' '#1095#1080#1089#1083#1072
+    TabOrder = 0
+    OnClick = btAddIntegerValuesClick
+  end
+  object btAddStringValues: TButton
+    Left = 16
+    Top = 88
+    Width = 137
+    Height = 41
+    Caption = #1057#1083#1086#1078#1080#1090#1100' '#1089#1090#1088#1086#1082#1080
+    TabOrder = 1
+    OnClick = btAddStringValuesClick
+  end
+  object edValue1: TEdit
+    Left = 248
+    Top = 52
+    Width = 121
+    Height = 21
+    TabOrder = 2
+  end
+  object edValue2: TEdit
+    Left = 248
+    Top = 98
+    Width = 121
+    Height = 21
+    TabOrder = 3
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,71 @@
+(*
+        Простой пример использования Полиморфизма в Delphi
+        на примере функции Add
+*)
+
+
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    btAddIntegerValues: TButton;
+    btAddStringValues: TButton;
+    edValue1: TEdit;
+    edValue2: TEdit;
+    procedure btAddIntegerValuesClick(Sender: TObject);
+    procedure btAddStringValuesClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+function Add(x,y:integer): integer; overload;
+begin
+  Result := x + y;
+end;
+
+function Add(x,y:string): string; overload;
+begin
+  Result := 'Итоговая строка: ' + x + y;
+end;
+
+{$R *.dfm}
+
+procedure TForm1.btAddIntegerValuesClick(Sender: TObject);
+var
+  ResultValue: string;
+  IntValue1, IntValue2: integer;
+begin
+            /// здесь, передаём в функцию Add числовые аргументы
+  try
+    IntValue1 := strtoint(trim(edValue1.Text));
+    IntValue2 := strtoint(trim(edValue2.Text));
+    ResultValue := inttostr ( Add(IntValue1, IntValue2) );
+    ShowMessage(ResultValue);
+  except
+    ShowMessage('Ошибка конвертации строка - число');
+  end;
+end;
+
+procedure TForm1.btAddStringValuesClick(Sender: TObject);
+var
+  ResultValue: string;
+begin
+            /// здесь, передаём в функцию Add строковые аргументы
+  ResultValue := Add(edValue1.Text, edValue2.Text);
+  ShowMessage(ResultValue);
+end;
+
+end.
